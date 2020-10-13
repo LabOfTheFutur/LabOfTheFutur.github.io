4687b6d06803983f2654e1036ae1c0f855f97497
Dmitry <dima76673@gmail.com>
2020-10-13 15:00:56 +0300

    Delete school

--- script.js
+++
@@ -1,9 +0,0 @@
-let stpbl1 = document.getElementById("stpbl1");
-let stpbrdr1 = document.getElementById("stpbrdr1");
-
-stpbl1.onmouseover = function(){
-	stpbrdr1.style.border-bottom = "3px solid white";
-}
-stpbl1.onmouseout = function(){
-	stpbrdr1.style.border-bottom = "3px solid black";
-}
